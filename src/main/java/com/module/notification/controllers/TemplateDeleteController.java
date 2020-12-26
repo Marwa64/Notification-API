2a2943ef967253a4cf6df3b1ce8f723d6e734224
Marwa64 <farabi.marwa@gmail.com>
2020-12-27 01:32:17 +0200

    controllers structure

---
+++ src/main/java/com/module/notification/controllers/TemplateDeleteController.java
@@ -0,0 +1,13 @@
+package com.module.notification.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.module.notification.notificationData.Template;
+
+@Controller
+public class TemplateDeleteController {
+
+}
