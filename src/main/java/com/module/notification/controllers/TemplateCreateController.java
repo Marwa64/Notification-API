b1d96646421804acce77eda7773e39ba7b982713
Marwa64 <farabi.marwa@gmail.com>
2020-12-27 11:47:27 +0200

    merge

--- src/main/java/com/module/notification/controllers/TemplateCreateController.java
+++ src/main/java/com/module/notification/controllers/TemplateCreateController.java
@@ -1,13 +1,33 @@
 package com.module.notification.controllers;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.module.notification.notificationData.Template;
 
-@Controller
+@RestController
 public class TemplateCreateController {
-
+	
+	//private final TemplateDataInterface templateDB = new TemplateDataFile();
+	
+	@PostMapping(
+		value = "/create/template",
+		consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}, //This is to allow use to accept
+		produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE} // and produce JSON and XML 
+			)
+	
+	/*@ResponseBody
+	public TemplateDataInterface saveTemplate(@RequestBody Template template) {
+		return TemplateDataFile.addTemplate(template);
+	}*/
+	@ResponseBody
+    public String postBody(@RequestBody String fullName) {
+        return "Hello " + fullName;
+    }
 }
--- src/main/java/com/module/notification/controllers/TemplateGetController.java
+++ src/main/java/com/module/notification/controllers/TemplateGetController.java
@@ -4,10 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.module.notification.notificationData.Template;
 
-@Controller
+@RestController
 public class TemplateGetController {
-
+	//http://localhost:8083/get/template
+	@RequestMapping(method = RequestMethod.GET, value = "/get/template")
+	
+	@ResponseBody
+	public String welcome() {
+		return "Welcome!";
+	}
 }
